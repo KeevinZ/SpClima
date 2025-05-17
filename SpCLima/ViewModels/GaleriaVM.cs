7efe9ef7493a6014587aeb1206196eb397595be0
keevinz <kevin1080p.mdm@mgmail.com>
2025-05-16 21:22:20 -0300

    Consertado o Erro de Sintaxe da ViewModels

--- SpCLima/ViewModels/GaleriaVM.cs
+++ SpCLima/ViewModels/GaleriaVM.cs
@@ -1,7 +1,7 @@
   using SpClima.Models;
 
 namespace SpCLima.ViewModels;
-public class ItemGaleria
+public class GaleriaVM
 {
     public ItemType Tipo { get; set; }
     public List<Item> Itens { get; set; }
